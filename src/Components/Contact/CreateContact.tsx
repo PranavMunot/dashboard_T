5855ddb7785ef2b565989f48ef4208aab4821463
PranavMunot <pranavmunot09@gmail.com>
2023-06-04 22:16:18 +0530

    commit 1

--- src/Components/Contact/CreateContact.tsx
+++ src/Components/Contact/CreateContact.tsx
@@ -3,7 +3,7 @@ import ContactForm from "./ContactForm";
 function CreateContact() {
   return (
     <div className="w-full bg-white-100">
-      <h1 className="text-2xl text-center">Create New Contact</h1>
+      <h1 className="text-2xl  text-center">Create New Contact</h1>
       <ContactForm isEdit={false} />
     </div>
   );
